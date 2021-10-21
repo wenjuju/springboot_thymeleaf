1de8fef767728b6ec4f3da4393dc15a05c1e6ecf
yixin.li <wen520143@126.com>
2021-10-21 10:32:40 +0800

    first commit

---
+++ src/main/java/com/lyxsdu/controller/GitTest.java
@@ -0,0 +1,8 @@
+package com.lyxsdu.controller;
+
+public class GitTest {
+    public static void main(String[] args) {
+        System.out.println("测试git功能的类");
+        System.out.println("hotfix——commit01");
+    }
+}
