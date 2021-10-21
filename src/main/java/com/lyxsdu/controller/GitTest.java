940f74d3b4090a8ddef7c01aa7db50cb4bfbe8eb
yixin.li <wen520143@126.com>
2021-10-21 14:32:04 +0800

    push20211021-commit01

--- src/main/java/com/lyxsdu/controller/GitTest.java
+++ src/main/java/com/lyxsdu/controller/GitTest.java
@@ -6,5 +6,6 @@ public class GitTest {
         System.out.println("hotfix——commit01");
         System.out.println("matst合并冲突测试commit01");
         System.out.println("hotfix--conmit02");
+        System.out.println("这是进行push操作010101==");
     }
 }
